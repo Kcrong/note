045cfa563a2eb8119a45e619bf017c89565180b9
revsic <revsic99@gmail.com>
2016-07-23 11:27:17 +0900

    Renew

--- 2016_07_22_reversing_s.py
+++ 2016_07_22_reversing_s.py
@@ -11,6 +11,36 @@ sock.send("test\n")
 
 sleep(0.1)
 sock.recv(1024)
-sock.send("123 456 789\n")
+sock.send("-1742237696 7368 -1344441807\n")
 
 sock.close()
+
+'''
+ebp+8 90000000
+ebp+c 8278cc8
+ebp+10
+
+eax = ebp+8 ^ ebp+c
+eax == 98278CC8
+ecx = ebp+c ^ ebp+10
+ecx == 0AFDD6EF9
+edx = ebp+8 + ebp+10
+edx == 48050231
+
+a ^ b = alpha
+b ^ c = beta
+c + a = theta
+
+a = alpha ^ b
+a = theta - c
+c = beta ^ b
+a = theta - beta ^ b
+
+alpha ^ b = theta - beta ^ b
+b = (theta - beta ^ b) ^ alpha;
+
+b = 0x1CC8
+a = 0x98279000
+c = afdd7231
+
+'''
